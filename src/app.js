94ead95fb53b3c163bd25d2823bac2dc6fcefdf6
lawrence-chege <lawrence.chege@meltwater.org>
2019-11-07 19:08:55 +0000

    add profile route

--- src/app.js
+++ src/app.js
@@ -36,7 +36,7 @@ app.get('/credit', (req, res)=>{
     res.render('account',{account: accounts.credit});
 });
 app.get('/profile', (req, res)=>{
-    res.render('profile',{ user: user[0]});
+    res.render('profile',{ user: users[0]});
 });
 
 app.listen(3000, ()=>console.log('Ps Project Running on port 3000'))
